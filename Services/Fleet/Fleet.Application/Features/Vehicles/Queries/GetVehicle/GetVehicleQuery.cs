51fcec1e12ed540a8b89249efd32c7280d7cecb5
Thabang Manaka <thabangdansteyn@gmail.com>
2026-08-18 21:27:36 +0200

    Added Queries

---
+++ Services/Fleet/Fleet.Application/Features/Vehicles/Queries/GetVehicle/GetVehicleQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fleet.Application.Features.Vehicles.Queries.GetVehicle
+{
+    internal class GetVehicleQuery
+    {
+    }
+}
---
+++ Services/Fleet/Fleet.Application/Features/Vehicles/Queries/GetVehicles/GetVehiclesQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fleet.Application.Features.Vehicles.Queries.GetVehicles
+{
+    internal class GetVehiclesQueryHandler
+    {
+    }
+}
