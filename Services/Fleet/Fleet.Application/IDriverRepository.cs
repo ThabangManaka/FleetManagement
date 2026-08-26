e753ac5a34eca1263353e06a83fdecae4c7f3100
Thabang Manaka <thabangdansteyn@gmail.com>
2026-08-26 20:15:15 +0200

    Added vehicle repository

---
+++ Services/Fleet/Fleet.Application/IDriverRepository.cs
@@ -0,0 +1,24 @@
+﻿using Fleet.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fleet.Application
+{
+    public interface IDriverRepository
+    {
+        Task<Driver?> GetByIdAsync(Guid id);
+
+        Task<List<Driver>> GetAllAsync();
+
+        Task AddAsync(Driver driver);
+
+        void Update(Driver driver);
+
+        void Delete(Driver driver);
+
+        Task<bool> ExistsAsync(Guid id);
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ Services/Fleet/Fleet.Infrastructure/Repositories/DriverRepository.cs
@@ -0,0 +1,56 @@
+﻿using Fleet.Application;
+using Fleet.Core.Entities;
+using Fleet.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fleet.Infrastructure.Repositories
+{
+    public class DriverRepository : IDriverRepository
+    {
+        private readonly FleetDbContext _context;
+
+        public DriverRepository(FleetDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Driver?> GetByIdAsync(Guid id)
+        {
+            return await _context.Drivers
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<Driver>> GetAllAsync()
+        {
+            return await _context.Drivers
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Driver driver)
+        {
+            await _context.Drivers.AddAsync(driver);
+        }
+
+        public void Update(Driver driver)
+        {
+            _context.Drivers.Update(driver);
+        }
+
+        public void Delete(Driver driver)
+        {
+            _context.Drivers.Remove(driver);
+        }
+
+        public async Task<bool> ExistsAsync(Guid id)
+        {
+            return await _context.Drivers
+                .AnyAsync(x => x.Id == id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
