23d45b2f8e9f481088222ca63394cf548745ffaf
Thabang Manaka <thabangdansteyn@gmail.com>
2026-08-26 20:08:02 +0200

    Added vehicle validators

---
+++ Services/Fleet/Fleet.Application/Features/Drivers/Validators/UpdateDriverValidator.cs
@@ -0,0 +1,45 @@
+﻿using Fleet.Application.Features.Drivers.DTOs;
+using FluentValidation;
+
+
+namespace Fleet.Application.Features.Drivers.Validators
+{
+    public class UpdateDriverValidator : AbstractValidator<UpdateDriverRequest>
+    {
+        public UpdateDriverValidator()
+        {
+            RuleFor(x => x.EmployeeNumber)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(200);
+
+            RuleFor(x => x.PhoneNumber)
+                .NotEmpty()
+                .MaximumLength(20);
+
+            RuleFor(x => x.LicenseNumber)
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(x => x.LicenseExpiryDate)
+                .GreaterThan(DateTime.UtcNow.Date)
+                .WithMessage("License expiry date must be in the future.");
+
+            RuleFor(x => x.Status)
+                .IsInEnum()
+                .WithMessage("Invalid driver status.");
+        }
+    }
+}
