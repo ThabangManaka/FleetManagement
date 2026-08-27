35638dac480ccd94216f7395fb69aebddbe8b039
Thabang Manaka <thabangdansteyn@gmail.com>
2026-08-27 18:01:45 +0200

    Get Drivers query

---
+++ Services/Fleet/Fleet.Application/Features/Vehicles/Queries/GetDrivers/GetDriversQuery.cs
@@ -0,0 +1,10 @@
+﻿using Fleet.Application.Features.Drivers.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fleet.Application.Features.Vehicles.Queries.GetDrivers
+{
+    public record GetDriversQuery : IRequest<List<DriverResponse>>;
+}
---
+++ Services/Fleet/Fleet.Application/Features/Vehicles/Queries/GetDrivers/GetDriversQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using Fleet.Application.Features.Drivers.DTOs;
+using MediatR;
+
+namespace Fleet.Application.Features.Vehicles.Queries.GetDrivers
+{
+    public class GetDriversQueryHandler
+    : IRequestHandler<GetDriversQuery, List<DriverResponse>>
+    {
+        private readonly IDriverRepository _driverRepository;
+
+        public GetDriversQueryHandler(
+            IDriverRepository driverRepository)
+        {
+            _driverRepository = driverRepository;
+        }
+
+        public async Task<List<DriverResponse>> Handle(
+            GetDriversQuery query,
+            CancellationToken cancellationToken)
+        {
+            var drivers = await _driverRepository.GetAllAsync();
+
+            return drivers
+                .Select(driver => new DriverResponse(
+                    driver.Id,
+                    driver.EmployeeNumber,
+                    driver.FirstName,
+                    driver.LastName,
+                    driver.Email,
+                    driver.PhoneNumber,
+                    driver.LicenseNumber,
+                    driver.LicenseExpiryDate,
+                    driver.Status,
+                    driver.CreatedAt,
+                    driver.UpdatedAt))
+                .ToList();
+        }
+    }
+}
