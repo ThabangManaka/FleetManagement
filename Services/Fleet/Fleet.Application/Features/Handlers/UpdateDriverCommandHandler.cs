7264ec5b6431bb5a7723c4e86a6275aa3ba6a7d3
Thabang Manaka <thabangdansteyn@gmail.com>
2026-08-27 18:04:37 +0200

    update driver commands

---
+++ Services/Fleet/Fleet.Application/Features/Handlers/UpdateDriverCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Fleet.Application.Features.Commands;
+using MediatR;
+
+namespace Fleet.Application.Features.Handlers
+{
+    public class UpdateDriverCommandHandler
+    : IRequestHandler<UpdateDriverCommand>
+    {
+        private readonly IDriverRepository _driverRepository;
+
+        public UpdateDriverCommandHandler(
+            IDriverRepository driverRepository)
+        {
+            _driverRepository = driverRepository;
+        }
+
+        public async Task Handle(
+            UpdateDriverCommand command,
+            CancellationToken cancellationToken)
+        {
+            var driver = await _driverRepository.GetByIdAsync(command.Id);
+
+            if (driver is null)
+            {
+                throw new KeyNotFoundException(
+                    $"Driver with ID '{command.Id}' was not found.");
+            }
+
+            driver.UpdateDetails(
+                command.Request.EmployeeNumber,
+                command.Request.FirstName,
+                command.Request.LastName,
+                command.Request.Email,
+                command.Request.PhoneNumber,
+                command.Request.LicenseNumber,
+                command.Request.LicenseExpiryDate,
+                command.Request.Status);
+
+            _driverRepository.Update(driver);
+
+            await _driverRepository.SaveChangesAsync();
+        }
+    }
+    
+}
