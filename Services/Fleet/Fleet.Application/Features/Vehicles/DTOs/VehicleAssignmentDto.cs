a30cb09f219e34523817825f0f3ed6554852272b
Thabang Manaka <thabangdansteyn@gmail.com>
2026-08-28 20:55:04 +0200

    updated vehicle assaignment dto

---
+++ Services/Fleet/Fleet.Application/Features/Vehicles/DTOs/VehicleAssignmentDto.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Fleet.Application.Features.Vehicles.DTOs
+{
+    public record VehicleAssignmentDto(
+    Guid Id,
+    Guid VehicleId,
+    Guid DriverId,
+    DateTime AssignedAt,
+    DateTime? UnassignedAt
+);
+}
---
+++ Services/Fleet/Fleet.Application/Features/Vehicles/Queries/GetVehicleAssignment/GetVehicleAssignmentQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fleet.Application.Features.Vehicles.Queries.GetVehicleAssignment
+{
+    public record GetVehicleAssignmentQuery(
+        Guid Id
+    ) : IRequest<VehicleAssignmentDto?>;
+}
