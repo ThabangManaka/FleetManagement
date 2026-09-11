5a6669bc67961ad1adc3eac8fa273c07081243f5
Thabang Manaka <thabangdansteyn@gmail.com>
2026-09-11 22:16:43 +0200

    added  trip delete command

---
+++ Services/Fleet/Fleet.Application/Features/Commands/DeleteTripCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Fleet.Application.Features.Commands
+{
+    public record DeleteTripCommand(
+    Guid Id
+) : IRequest;
+}
---
+++ Services/Fleet/Fleet.Application/Features/Handlers/DeleteTripCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Fleet.Application.Features.Commands;
+using Fleet.Application.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fleet.Application.Features.Handlers
+{
+    public class DeleteTripCommandHandler
+         : IRequestHandler<DeleteTripCommand>
+    {
+        private readonly ITripRepository _tripRepository;
+
+        public DeleteTripCommandHandler(
+            ITripRepository tripRepository)
+        {
+            _tripRepository = tripRepository;
+        }
+
+        public async Task Handle(
+            DeleteTripCommand command,
+            CancellationToken cancellationToken)
+        {
+            var trip = await _tripRepository.GetByIdAsync(
+                command.Id,
+                cancellationToken);
+
+            if (trip is null)
+            {
+                throw new KeyNotFoundException(
+                    $"Trip with ID '{command.Id}' was not found.");
+            }
+
+            _tripRepository.Delete(trip);
+
+            await _tripRepository.SaveChangesAsync(
+                cancellationToken);
+        }
+    }
+}
