d0fb1c8d37e17e686c57ce59519a986376c5133c
Thabang Manaka <thabangdansteyn@gmail.com>
2026-09-11 21:48:41 +0200

    added  trip update command

---
+++ Services/Fleet/Fleet.Application/Features/Commands/UpdateTripCommand.cs
@@ -0,0 +1,11 @@
+﻿using Fleet.Application.Features.Trips.DTOs;
+using MediatR;
+
+
+namespace Fleet.Application.Features.Commands
+{
+    public record UpdateTripCommand(
+     Guid Id,
+     UpdateTripRequest Request
+ ) : IRequest;
+}
