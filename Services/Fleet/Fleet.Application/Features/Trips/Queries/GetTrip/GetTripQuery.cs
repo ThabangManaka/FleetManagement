31a06ef503de2abfc8e78b973fe8b61327af74bf
Thabang Manaka <thabangdansteyn@gmail.com>
2026-09-11 22:25:32 +0200

    added  trip queries

---
+++ Services/Fleet/Fleet.Application/Features/Trips/Queries/GetTrip/GetTripQuery.cs
@@ -0,0 +1,10 @@
+﻿using Fleet.Application.Features.Trips.DTOs;
+using MediatR;
+
+
+namespace Fleet.Application.Features.Trips.Queries.GetTrip
+{
+    public record GetTripQuery(
+         Guid Id
+     ) : IRequest<TripResponse?>;
+}
---
+++ Services/Fleet/Fleet.Application/Features/Trips/Queries/GetTrip/GetTripQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fleet.Application.Features.Trips.Queries.GetTrip
+{
+    internal class GetTripQueryHandler
+    {
+    }
+}
