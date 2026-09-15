2687174ddd4ae00951cb280d037ec3d0b3c45fcd
Thabang Manaka <thabangdansteyn@gmail.com>
2026-09-15 16:16:44 +0200

    added get fuel transa query handler

---
+++ Services/Fleet/Fleet.Application/Features/FuelTransactions/Queries/GetFuelTransactionsQuery.cs
@@ -0,0 +1,8 @@
+﻿using Fleet.Application.Features.FuelTransactions.DTOs;
+using MediatR;
+
+namespace Fleet.Application.Features.FuelTransactions.Queries
+{
+    public record GetFuelTransactionsQuery
+       : IRequest<List<FuelTransactionResponse>>;
+}
