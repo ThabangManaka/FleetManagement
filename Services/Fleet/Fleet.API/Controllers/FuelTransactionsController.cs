6178fd2d9471538e1b8a6fc11260d45127d785d0
Thabang Manaka <thabangdansteyn@gmail.com>
2026-09-15 16:19:36 +0200

    added fuel transaction  controller

---
+++ Services/Fleet/Fleet.API/Controllers/FuelTransactionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fleet.API.Controllers
+{
+    public class FuelTransactionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
