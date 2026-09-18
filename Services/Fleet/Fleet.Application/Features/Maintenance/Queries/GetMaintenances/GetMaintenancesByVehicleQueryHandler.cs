3d6dcd91d15381a7c37d7296a56936ac37c920f9
Thabang Manaka <thabangdansteyn@gmail.com>
2026-09-18 18:17:30 +0200

    Added get mainteenance by vehicle query handle

---
+++ Services/Fleet/Fleet.Application/Features/Maintenance/Queries/GetMaintenances/GetMaintenancesByVehicleQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using Fleet.Application.Interfaces;
+using MediatR;
+
+
+namespace Fleet.Application.Features.Maintenance.Queries.GetMaintenances
+{
+    public class GetMaintenancesByVehicleQueryHandler
+     : IRequestHandler<
+         GetMaintenancesByVehicleQuery,
+         List<MaintenanceResponse>>
+    {
+        private readonly IMaintenanceRepository _maintenanceRepository;
+
+        public GetMaintenancesByVehicleQueryHandler(
+            IMaintenanceRepository maintenanceRepository)
+        {
+            _maintenanceRepository = maintenanceRepository;
+        }
+
+        public async Task<List<MaintenanceResponse>> Handle(
+            GetMaintenancesByVehicleQuery query,
+            CancellationToken cancellationToken)
+        {
+            var maintenances =
+                await _maintenanceRepository.GetByVehicleIdAsync(
+                    query.VehicleId,
+                    cancellationToken);
+
+            return maintenances
+                .Select(x => new MaintenanceResponse(
+                    x.Id,
+                    x.VehicleId,
+                    x.MaintenanceType,
+                    x.Description,
+                    x.ServiceDate,
+                    x.Mileage,
+                    x.Cost,
+                    x.Notes,
+                    x.Status,
+                    x.CreatedAt,
+                    x.UpdatedAt))
+                .ToList();
+        }
+    }
+}
