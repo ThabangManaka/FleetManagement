b4bebaf2a8f7053acfe8df49e22d3a8c6b834922
Thabang Manaka <thabangdansteyn@gmail.com>
2026-09-19 19:27:04 +0200

    Added Fleet Dashboard Response

---
+++ Services/Fleet/Fleet.Application/Features/FleetDashboard/DTOs/FleetDashboardResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fleet.Application.Features.FleetDashboard.DTOs
+{
+    public record FleetDashboardResponse(
+      int TotalVehicles,
+      int AvailableVehicles,
+      int AssignedVehicles,
+      int VehiclesInMaintenance,
+      int TotalDrivers,
+      int ActiveDrivers,
+      decimal TotalFuelCost,
+      decimal TotalMaintenanceCost,
+      int TotalTrips,
+      decimal TotalDistanceTravelled
+  );
+}
