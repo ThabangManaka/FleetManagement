c04e16a5e0d1cdb79f09adbc1473a7c2bb0cf69e
Thabang Manaka <thabangdansteyn@gmail.com>
2026-09-19 20:38:28 +0200

    Added Fleet Dashboard controller

---
+++ Services/Fleet/Fleet.API/Controllers/FleetDashboardController.cs
@@ -0,0 +1,29 @@
+﻿using Fleet.Application.Features.FleetDashboard.Queries.GetFleetDashboard;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fleet.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FleetDashboardController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public FleetDashboardController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDashboard(
+            CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(
+                new GetFleetDashboardQuery(),
+                cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
