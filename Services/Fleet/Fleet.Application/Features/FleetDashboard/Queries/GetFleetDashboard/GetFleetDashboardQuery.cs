85bd571a4d094dd61f05f27577ad8d4d3a0b2a5e
Thabang Manaka <thabangdansteyn@gmail.com>
2026-09-19 19:31:07 +0200

    Added Fleet Dashboard query

---
+++ Services/Fleet/Fleet.Application/Features/FleetDashboard/Queries/GetFleetDashboard/GetFleetDashboardQuery.cs
@@ -0,0 +1,11 @@
+﻿using Fleet.Application.Features.FleetDashboard.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fleet.Application.Features.FleetDashboard.Queries.GetFleetDashboard
+{
+    public record GetFleetDashboardQuery
+        : IRequest<FleetDashboardResponse>;
+}
